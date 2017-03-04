66051df73a83a5da53e795f5996f0dc6f6792602
gregmarquet <marquet.greg@gmail.com>
2017-03-04 15:42:59 -0500

    add giph_detail, giph_list and giph_lits_item files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import request from 'superagent';
 
-const App = () => {
-  return <div>HELLO WORD!</div>;
+const url = `http://api.giphy.com/v1/gifs/search?q=money&api_key=dc6zaTOxFJmzC`;
+
+request.get(url, function(err, res) {
+  console.log(res.body.data);
+})
+
+const App = () => { 
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
